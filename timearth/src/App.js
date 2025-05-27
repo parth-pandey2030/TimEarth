47124778e249fcdcd1003d863ac068e72fe61a19
Parth Pandey <147671693+parth-pandey2030@users.noreply.github.com>
2025-05-27 15:22:45 +0000

    Tue May 27 15:22:45 UTC 2025

--- timearth/src/App.js
+++ timearth/src/App.js
@@ -6,6 +6,7 @@ function App() {
             <h1> Welcome to TimEarth (pronounced "time-earth")! </h1>
             <canvas id = "currentpath" width={500} height={300}></canvas>
             <p id = "sources">
+            
             Sources
             <a href = "https://en-roads.climateinteractive.org/scenario.html?v=25.5.0">En-ROADS</a>
             </p>
--- timearth/src/drawgraph.js
+++ timearth/src/drawgraph.js
@@ -1,1 +1,15 @@
-const ctx = document.getElementById("currentpath").getContext("2d");
+import { currentpath } from "./data";
+
+export const ctx = document.getElementById("currentpath").getContext("2d");
+export function drawPoint(x, y, size = 1, color = 'black') {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, size, size);
+}
+
+export default function DrawCurrentGraph() {
+    for (let i = 25; i <= 100; i++) {
+        i += 2000;
+        let gigatons = currentpath[i.toString()];
+        drawPoint(i, gigatons);
+    }
+}
