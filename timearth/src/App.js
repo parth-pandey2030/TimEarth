d093345c6dbc1206536275565aa01157d3d4ab47
Parth Pandey <147671693+parth-pandey2030@users.noreply.github.com>
2025-05-27 20:31:07 +0000

    Tue May 27 20:31:07 UTC 2025

--- timearth/src/App.js
+++ timearth/src/App.js
@@ -1,16 +1,29 @@
-
+import { netzerotimes } from "./data"; 
+import DrawCurrentGraph from "./drawgraph";
+import { useRef, useEffect } from 'react';
 
 function App() {
+    const graphRef = useRef(null);
+
+    useEffect(() => {
+        const graph = graphRef.current;
+        if (graph) {
+            // TODO: Add rendering data
+        }
+    })
     return (
-        <>
+        <div>
             <h1> Welcome to TimEarth (pronounced "time-earth")! </h1>
-            <canvas id = "currentpath" width={500} height={300}></canvas>
-            <p id = "sources">
+            <canvas ref={graphRef}></canvas>
+
             
+                
+            
+            <p id = "sources">
             Sources
             <a href = "https://en-roads.climateinteractive.org/scenario.html?v=25.5.0">En-ROADS</a>
             </p>
-        </>
+        </div>
     )
 }
 
--- timearth/src/drawgraph.js
+++ timearth/src/drawgraph.js
@@ -1,15 +1,14 @@
 import { currentpath } from "./data";
 
-export const ctx = document.getElementById("currentpath").getContext("2d");
-export function drawPoint(x, y, size = 1, color = 'black') {
+export function drawPoint(x, y, ctx, size = 1, color = 'black') {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, size, size);
 }
 
-export default function DrawCurrentGraph() {
+export default function DrawCurrentGraph(ctx) {
     for (let i = 25; i <= 100; i++) {
         i += 2000;
         let gigatons = currentpath[i.toString()];
-        drawPoint(i, gigatons);
+        drawPoint(i, gigatons, ctx);
     }
 }
